6c729e7e838e3e8959c3bcc5df5c6d247e1c05c4
obleus <odney@Odneys-MacBook-Air.local>
2022-06-13 17:36:51 -0400

    heroku commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/IMG_0611-removebg.png";
+import ME from "../../assets/IMG_0610-removebg.png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
