0da0be269ab56351523321fbbc9a116c3d9a9a5d
obleus <odney@Odneys-MacBook-Air.local>
2022-06-13 16:04:24 -0400

    added footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './footer.css'
-
+import {AiFillLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer id='footer'>
+      
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#reference'>Reference</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className="footer_socials">
+        <a href='https://www.linkedin.com/in/odney-bleus-580ab61b6/' target='_blank'><AiFillLinkedin/></a>
+        <a href='https://github.com/obleus' target='_blank'><AiFillGithub/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy;Odney Bleus. All Rights Reserved.</small>
+      </div>
+    </footer>
   )
 }
 
