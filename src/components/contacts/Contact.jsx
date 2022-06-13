c3a0ae351fd1fc950d117b002f9e7d3ccce8cf55
obleus <odney@Odneys-MacBook-Air.local>
2022-06-13 01:06:49 -0400

    added contact component

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './contatc.css'
+import {MdMarkEmailUnread} from 'react-icons/md'
+import {BsFillPhoneFill} from 'react-icons/bs'
+
+
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdMarkEmailUnread/>
+            <h4>Email</h4>
+            <h5>Odneyb@yahoo.com</h5>
+            <a href='mailto:odneyb@yahoo.com'>Send A Message</a>
+          </article>
+          <article className="contact_option">
+            <BsFillPhoneFill/>
+            <h4>Phone</h4>
+            <h5>(561)-718-5485</h5>
+            <a href='sms:561-718-5485'>Send A Text</a>
+          </article>
+        </div>
+        <form action="">
+          <input type='text' name='name' placeholder='Your Full Name' required />
+          <input type='email' name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
