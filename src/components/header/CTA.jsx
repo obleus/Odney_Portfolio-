c4a230b49070462aec0cffb935bca7a0f7759b68
obleus <odney@Odneys-MacBook-Air.local>
2023-07-01 00:46:12 -0400

    updated resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CV from '../../assets/odneybleusRESUME.pdf'
+import CV from '../../assets/odneybleus2023.pdf'
 
 const CTA = () => {
   return (
     <div>
         <div className="cta">
-            <a href={CV} download className='btn'>Download CV</a>
+            <a href={CV} download className='btn'>Download Resume</a>
             <a href='#contact' className='btn btn-primary'>Let's Connect</a>
         </div>
     </div>
